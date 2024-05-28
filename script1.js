afa803150466a06de5fa25f4c9c95f588dd077b5
JayT <161474719+Rumours-FM@users.noreply.github.com>
2024-05-28 14:24:48 +0800

    update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.timeline()
-.from('.navbar-brand',{opacity:0, duration:1.5, y:-50, ease:"slow"}).from('.heroimage', {opacity:0, duration:2, x:100, delay:1 ,ease:"slow"});
+gsap.from('.navbar-brand',{opacity:0, duration:1.5, y:-50, ease:"slow"})
+gsap.from('.heroimage', {opacity:0, duration:2, x:100, delay:0.5 ,ease:"slow"});
        
 gsap.from(".storiesquote", {
         scrollTrigger:".storiesbg",
@@ -12,3 +12,12 @@ gsap.from(".storiesquote", {
         scrub:1,
         });
     
+
+gsap.from(".learnaboutquote", {
+        scrollTrigger:".learnaboutbg",
+        duration:2,
+            x:250,
+            opacity:0,
+        start:400,
+        scrub:1,
+        });
